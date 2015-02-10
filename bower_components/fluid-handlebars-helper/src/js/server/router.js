f2879dae58b4a5512448e300cbb81cbf4de776d8
Tony <tony@raisingthefloor.org>
2015-02-10 11:02:15 +0100

    Removed checked-in dependencies

--- bower_components/fluid-handlebars-helper/src/js/server/router.js
+++
@@ -1,6 +0,0 @@
-var fluid = fluid || require('infusion');
-
-// "marker" grade for components that act as an express router
-fluid.defaults("gpii.express.router", {
-    gradeNames: ["fluid.eventedComponent", "autoInit"]
-});
